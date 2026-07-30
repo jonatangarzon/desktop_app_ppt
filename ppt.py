fa7bd22085531b2224d9b6638d12788759ee8fe3
jonatan javier garzon nieto <jonatangarzon@colegioguanenta.edu.co>
2026-07-30 18:26:50 -0500

    Add files via upload

---
+++ ppt.py
@@ -0,0 +1,34 @@
+import cmath
+import tkinter
+from tkinter import messagebo
+
+
+
+
+
+
+
+
+
+# --- Ventana principal ---
+
+ventana_principal = tkinter.Tk()
+
+# titulo
+ventana_principal.title("Ecuación Cuadrática - Dark Mode")
+
+# tamaño de la vcentana
+ventana_principal.geometry("500x500")
+
+# color de fondo
+ventana_principal.config(bg="#121212")  # Fondo general súper oscuro
+
+# icono de la ventana
+ventana_principal.resizable(0, 0)
+
+
+
+
+
+# Bucle principal
+ventana_principal.mainloop()
